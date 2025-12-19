5f6aa4fe98ae08a4845b1c7c889c1a604e715974
kleislicat <79645287+kleislicat@users.noreply.github.com>
2025-12-19 15:27:01 +0000

    feat: Refresh configs at runtime using Spring Cloud Config Monitor

--- src/main/java/com/kleislicat/accounts/dto/AccountsContactInfoDto.java
+++ src/main/java/com/kleislicat/accounts/dto/AccountsContactInfoDto.java
@@ -3,10 +3,17 @@ package com.kleislicat.accounts.dto;
 
 import java.util.List;
 import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message,
-                                     Map<String, String> contactDetails,
-                                     List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountsContactInfoDto {
+
+  private String message;
+  private Map<String, String> contactDetails;
+  private List<String> onCallSupport;
+
 }
