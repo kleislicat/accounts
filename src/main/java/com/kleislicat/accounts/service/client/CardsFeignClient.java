36ab86feb34b82bf9f362459e8b8cca09d0ef864
kleislicat <79645287+kleislicat@users.noreply.github.com>
2025-12-30 11:20:18 +0000

    feat: Implement Service discovery and registration with Eureka

---
+++ src/main/java/com/kleislicat/accounts/service/client/CardsFeignClient.java
@@ -0,0 +1,14 @@
+package com.kleislicat.accounts.service.client;
+
+import com.kleislicat.accounts.dto.CardsDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("cards")
+public interface CardsFeignClient {
+
+  @GetMapping(value = "/api/fetch", consumes = "application/json")
+  public ResponseEntity<CardsDto> fetchCardDetails(@RequestParam String mobileNumber);
+}
